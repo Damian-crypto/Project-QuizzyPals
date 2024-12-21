20122f79182b0d8a3c797b8dda87d38d80f5a113
Damian Chamel <58256720+Damian-crypto@users.noreply.github.com>
2024-12-21 11:24:57 +0530

    Feature/two timers (#81)

--- frontend/src/pages/SummaryPage.js
+++ frontend/src/pages/SummaryPage.js
@@ -26,30 +26,65 @@ const SummaryPage = () => {
   };
 
   const getAndSetQuestions = async () => {
+    // await axios
+    //   .get(
+    //     `${process.env.REACT_APP_BASE_URL}/api/game/getquizzes/${user.userId}`,
+    //     requestHeaders
+    //   )
+    //   .then((response) => {
+    //     if (response.status === 200) {
+    //       const tmpQAndA = [];
+    //       for (let qa of response.data) {
+    //         // Q&A for rendering
+    //         tmpQAndA.push({
+    //           owner: user.email,
+    //           question: qa.question,
+    //           answers: JSON.parse(qa.answer),
+    //           correctAnswer: qa.correct || "No correct answer provided",
+    //         });
+    //       }
+
+    //       setQuestions(tmpQAndA);
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     setError(error);
+    //   });a
+
     await axios
       .get(
         `${process.env.REACT_APP_BASE_URL}/api/game/get-all-player-answers/${room.roomId}`,
+        // `${process.env.REACT_APP_BASE_URL}/api/game/get-all-player-answers/6765cc0fd3ef2d4bbfe1cf45`,
         requestHeaders
       )
       .then((response) => {
         console.log(response);
         if (response.status === 200) {
-          console.log("all player answers", response.data);
-          const tmpQAndA = [];
-          for (let qa of response.data) {
-            if (qa.answeredBy === user.email) {
-              continue;
-            }
-            tmpQAndA.push({
-              owner: qa.createdBy,
-              question: Object.keys(qa.questionAndAnswer)[0],
-              answers:
-                Object.values(qa.questionAndAnswer)[0] ||
-                "No correct answer provided",
+          // console.log("all player answers", response.data);
+          const qaMap = new Map();
+          for (const qa of response.data) {
+            qaMap[qa.question] = {
+              question: qa.question,
+              answers: [],
               correctAnswer: qa.correctAnswer,
+              owner: qa.createdBy,
+            };
+          }
+
+          for (const qa of response.data) {
+            qaMap[qa.question].answers.push({
+              answer: qa.answer,
               answeredBy: qa.answeredBy,
             });
           }
+
+          const tmpQAndA = [];
+          for (let item of Object.keys(qaMap)) {
+            tmpQAndA.push(qaMap[item]);
+          }
+
+          // console.log(qaMap);
           setQuestions(tmpQAndA);
         }
       })
@@ -58,6 +93,28 @@ const SummaryPage = () => {
         setError(error);
       });
   };
+
+  // Sample questions data
+  // const questions = [
+  //   {
+  //     owner: "Owner 1",
+  //     question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+  //     correctAnswer: "Suspendisse aliquam et augue sit amet euismod",
+  //     answers: ["Q1 Answer 1", "Q1 Answer 2", "Q1 Answer 3"],
+  //   },
+  //   {
+  //     owner: "Owner 2",
+  //     question: "Quisque non leo at velit commodo suscipit?",
+  //     correctAnswer: "Nullam scelerisque turpis a libero vulputate suscipit",
+  //     answers: ["Q2 Answer 1", "Q2 Answer 2", "Q2 Answer 3"],
+  //   },
+  //   {
+  //     owner: "Owner 3",
+  //     question: "Phasellus sollicitudin quam eget velit feugiat?",
+  //     correctAnswer: "Etiam sit amet tortor a mi aliquam tincidunt",
+  //     answers: ["Q3 Answer 1", "Q3 Answer 2", "Q3 Answer 3"],
+  //   },
+  // ];
 
   // State for the current question index
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -101,6 +158,7 @@ const SummaryPage = () => {
       <div className="summary-header">
         <div className="summary-header-left">
           <div className="room-code">Room: {room && room.roomId}</div>
+          {/* <p>I am {user.email}</p> */}
           <div className="round-title">SUMMARY</div>
         </div>
       </div>
@@ -127,11 +185,16 @@ const SummaryPage = () => {
               <div className="correct-answer">
                 <b>Correct answer: {currentQuestion.correctAnswer}</b>
               </div>
+              {/* <div className="correct-answer">
+                <b>Your answer: {playerAnswers[currentQuestion.question]}</b>
+              </div> */}
               <div className="answer-list-container">
-                <div className="answer-list">
-                  <p>{currentQuestion.answeredBy}</p>
-                  <div className="user-answer-container">
-                    <p>{currentQuestion.answers}</p>
+                {currentQuestion.answers.map((answer, index) => (
+                  <div className="answer-list" key={index}>
+                    <p>{answer.answeredBy}</p>
+                    <div className="user-answer-container">
+                      <p>{answer.answer}</p>
+                    </div>
                   </div>
                 </div>
               </div>
